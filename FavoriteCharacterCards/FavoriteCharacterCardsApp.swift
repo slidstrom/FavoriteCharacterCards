64222870f8bc67242ab185c0063630288958b0dc
slidstrom <spencerlidstrom@gmail.com>
2021-12-16 14:48:05 -0600

    Initial Commit

---
+++ FavoriteCharacterCards/FavoriteCharacterCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteCharacterCardsApp.swift
+//  FavoriteCharacterCards
+//
+//  Created by DSIAdmin on 12/16/21.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteCharacterCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
